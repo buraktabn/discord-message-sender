08c9fba7f62acb1413681edebf3217a5e3996d63
Burak <burak.tabn@gmail.com>
2020-09-09 15:11:54 -0400

    added README

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const axios = require('axios').default;
 const txtgen = require('txtgen');
+var credentials = require('./credentials.json')
 
-const channelID = '********';
-const authorization = '**********';
-
-const baseURL = `https://discord.com/api/v8/channels/${channelID}/messages`
+const baseURL = `https://discord.com/api/v8/channels/${credentials.channelID}/messages`
 const payload = { content: "2+2=5", tts: false }
 
 setInterval(async () => {
 
     payload.content = Math.floor(Math.random() * 10) < 5 ? txtgen.sentence() : txtgen.paragraph()
 
-    await axios.post(baseURL, payload, { headers: { 'authorization': authorization } })
+    await axios.post(baseURL, payload, { headers: { 'authorization': credentials.authorization } })
     console.log('sent.');
 }, 1000 * 60 + 10)
